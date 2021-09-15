1057092af2f956e239aec2b6f096c1d5ea83e6a1
klee128 <klee128@ucsc.edu>
2021-09-14 22:09:35 -0700

    create Product interface. In service class, create functions to add product, delete product, and display products. edit shopping-cart and add-groceries to display products using service class function

--- Frontend/GroceryStore/src/app/customer/add-groceries/add-groceries.component.ts
+++ Frontend/GroceryStore/src/app/customer/add-groceries/add-groceries.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EditCartService } from '../edit-cart.service';
+import { Product } from '../product';
 
 @Component({
   selector: 'app-add-groceries',
@@ -8,61 +9,19 @@ import { EditCartService } from '../edit-cart.service';
 })
 export class AddGroceriesComponent implements OnInit {
   // variables
-  productsArray?: Array<any>
+  productsArray: Array<Product> = [];
 
   constructor(public editCartSer: EditCartService) { }
 
   ngOnInit(): void {
-    this.getAllProducts();
-  }
-
-
-  getAllProducts() {
-    // call service class's displayAllProducts function
-    // store result in variable: productsArray
-    this.editCartSer.displayAllProducts().subscribe(result => {
+    // this.getAllProducts();
+    this.editCartSer.getAllProducts().subscribe(result => {
       this.productsArray = result;
-    })
-  }
-
-  // input: product name
-  // add product to shopping cart (stored in localStorage)
-  addToCart(pName: string) {
-    // get shopping cart from localStorage
-    let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart") || "");
-    console.log(shoppingCart); //delete
-    function findProduct(product: any) { return product.productName == pName; }
-
-    // if product is already in shopping cart, increment product quantity by 1
-    // else, add product to shopping cart for the first time
-    if (shoppingCart.find(findProduct)) {
-      shoppingCart.find(findProduct).quantity += 1;
-    } else {
-      shoppingCart.push({ productName: pName, quantity: 1 });
-    }
-    // store updated shopping cart into localStorage
-    localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
-  }
-
-  // delete product from shopping cart
-  deleteFromCart(pName: string) {
-    let shoppingCart = JSON.parse(localStorage.getItem("shoppingCart") || "");
-    function findProduct(product: any) { return product.productName == pName; }
-
-    // if product in shopping cart
-    let foundProductIdx = shoppingCart.findIndex(findProduct);
+      // use service class's function to display products 
+      this.editCartSer.displayProducts(this.productsArray);
+    });
     
-    if (foundProductIdx >= 0) {
-      if (shoppingCart[foundProductIdx].quantity == 1) {
-        shoppingCart.splice(foundProductIdx, 1);
-      }
-      else if (shoppingCart[foundProductIdx].quantity > 1) {
-        shoppingCart[foundProductIdx].quantity -= 1;
-      }
-      localStorage.setItem("shoppingCart", JSON.stringify(shoppingCart));
-    }
-    
-    console.log(shoppingCart);
   }
+
 
 }
--- Frontend/GroceryStore/src/app/customer/customer.component.ts
+++ Frontend/GroceryStore/src/app/customer/customer.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  // variables
+  clicked_AddGroceries: boolean = true;
+  clicked_ViewShoppingCart: boolean = false;
 
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showAddGroceries() {
+    this.clicked_AddGroceries = true;
+    this.clicked_ViewShoppingCart = false;
+  }
+
+  showViewShoppingCart() {
+    this.clicked_AddGroceries = false;
+    this.clicked_ViewShoppingCart = true;
   }
 
 }
