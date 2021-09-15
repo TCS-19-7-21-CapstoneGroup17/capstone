e0c292900de3d09c07486d6530aeb49c15c9e660
klee128 <klee128@ucsc.edu>
2021-09-15 11:17:52 -0700

    add ticket table

--- Backend/app.js
+++ Backend/app.js
@@ -8,6 +8,7 @@ let employeeRouter = require('./router/employee.router');
 let productRouter = require('./router/product.router');
 let userRouter = require('./router/user.router');
 let adminRouter = require('./router/admin.router');
+let ticketRouter = require('./router/ticket.router');
 
 //create express reference and add middleware
 let app = express();
@@ -17,6 +18,7 @@ app.use("/admin", employeeRouter);
 app.use("/product", productRouter);
 app.use("/user", userRouter);
 app.use("/admin", adminRouter);
+app.use("/ticket", ticketRouter); 
 
 //connect the database
 mongoose.connect("mongodb://localhost:27017/capstone").
